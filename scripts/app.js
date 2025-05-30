09da1d7b9ed6b870f0b5b15669bc02da74e35665
Tafi Banda <30298378+niggalodon@users.noreply.github.com>
2025-05-30 20:29:07 +0200

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+function mobileNav() {
+  if (document.getElementById("mobile-navigation").style.display == "flex") {
+    document.getElementById("mobile-navigation").style.display = "none";
+  } else {
+    document.getElementById("mobile-navigation").style.display = "flex";
+  }
+}
+
+function mobileNavClose() {
+  document.getElementById("mobile-navigation").style.display = "none";
+}
+
+document.getElementById("mobile-navigation-toggle").addEventListener("click", mobileNav);
+document.getElementById("mobile-navigation").addEventListener("click", mobileNavClose);
+document.getElementById("mobile-navigation-close").addEventListener("click", mobileNavClose);
